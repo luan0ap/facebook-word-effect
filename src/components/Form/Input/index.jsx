e8785f7fae42d69e5a5bd1f50fa6b1bcd102f213
luan0ap <luanap.kun@gmail.com>
2018-06-21 15:15:50 -0300

    Created main components

---
+++ src/components/Form/Input/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Input = ({ handleChange }) =>
+  <React.Fragment>
+    <label htmlFor="words">Facebook</label>
+    <textarea 
+    name="words" 
+    id="word" 
+    cols="30" 
+    rows="10" 
+    className="input" 
+    onChange={handleChange}>
+    </textarea>
+  </ React.Fragment>
+
+export default Input
